da399ac81b9a526377c073753abc27871c996adc
JeffreyTangMain <97859002+JeffreyTangMain@users.noreply.github.com>
2022-02-09 21:48:24 -0500

    1.1: Removed if statement
    YouTube changes URLs with AJAX, so the userscript doesn't reload on page changes, meaning the userscript will end itself once and forever.
    I just made it run forever. It's not a good fix, but it's better than being broken.

--- Scripts/Auto-Expand Description.user.js
+++ Scripts/Auto-Expand Description.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto-Expand Description
 // @namespace    https://www.youtube.com/
-// @version      1.0
+// @version      1.1
 // @description  Clicks "Show More" on the description of videos on page load.
 // @author       Main
 // @match        https://www.youtube.com/*
@@ -11,13 +11,11 @@
 // Documenting globals for JSHint to not throw an error
 /* globals waitForKeyElements $ */
 
-waitForKeyElements("yt-formatted-string[slot^='more-button']", descClicker);
+waitForKeyElements(".ytd-video-secondary-info-renderer", descClicker);
 
 function descClicker(){
     setTimeout(function() {
-        if($("yt-formatted-string[slot^='more-button']:visible").length != 0){
-            $("yt-formatted-string[slot^='more-button']:visible").click();
-            descClicker();
-        }
+        $("yt-formatted-string[slot^='more-button']:visible").click();
+        descClicker();
     }, 50);
 }
