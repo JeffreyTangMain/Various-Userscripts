5a041e44a8aa6cb625c5f06afba20f414ae093c4
JeffreyTangMain <97859002+JeffreyTangMain@users.noreply.github.com>
2023-02-10 18:34:31 -0500

    3.15: Better checks for drops
    Now the script is capable of reading the console by hijacking console.log to check if the drops or missions are enabled.
    It very specifically checks for drops OR missions, so it's possible for it to get into a situation where there isn't a drop but there is a mission it can't complete.
    I don't know how to work around that, so this is fine for now.
    
    The 10 minute timer has been completely removed and the 10 second timer is only used for YouTube because that's the one that autopauses at random.
    The 10 minute timer SHOULD be unnecessary now that the script can read chat for the enabled drops directly, which might allow it to refresh immediately when drops stop showing up. This depends on the website properly updating that it can no longer get drops, which might not happen. It's hard to test since it's entirely dependent on hour long stream cycles.
    
    The script is also capable of going through every live stream in order instead of picking whichever link loaded first.

--- Scripts/LoL Esports Redirector.user.js
+++ Scripts/LoL Esports Redirector.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         LoL Esports Redirector
 // @namespace    https://lolesports.com/
-// @version      3.14
+// @version      3.15
 // @description  Redirects the schedule to the livestream so you're always watching when it's available.
 // @author       Main
 // @match        https://lolesports.com/schedule*
 // @match        https://lolesports.com/live/*
+// @grant GM_setValue
+// @grant GM_getValue
 // @require http://code.jquery.com/jquery-3.4.1.min.js
 // @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js
 // ==/UserScript==
@@ -13,6 +15,31 @@
 /* globals $ waitForKeyElements */
 
 var redirectPathCheck = '/live';
+var containerLoaded = false;
+var tempString = '';
+
+var oldLog = unsafeWindow.console.log;
+
+unsafeWindow.console.log = function(msg) {
+    try {
+        tempString = arguments[5];
+        if(tempString != undefined && tempString.includes('mission') && !(tempString.includes('mission=on') || tempString.includes('drop=on'))){
+            window.location.href = 'https://lolesports.com/schedule';
+        }
+        tempString = arguments[5];
+        if(tempString != undefined && tempString.includes('WatchLive')){
+            containerLoaded = true;
+        }
+        tempString = arguments[2];
+        if(tempString != undefined && tempString.includes('VideoPlayer') && !(tempString.includes('Twitch'))){
+            // Check for the YouTube embed instead of the Twitch embed. Set up the timer later down to work in case it is.
+            containerLoaded = false;
+        }
+    } catch (error) {
+        null;
+    }
+    oldLog.apply(null, arguments);
+}
 
 if(window.location.toString().indexOf('/schedule') != -1){
     waitForKeyElements('.Event', mainMethod);
@@ -21,13 +48,21 @@ if(window.location.toString().indexOf('/schedule') != -1){
 }
 
 function mainMethod(){
-    liveClicker(function(){setTimeout(function(){window.location.href = 'https://lolesports.com/schedule'}, 600000)});
+    liveClicker(function(){null;});
 }
 
 function liveClicker(method, loop){
-    var liveButton = document.querySelector (
-        'a.live'
-    );
+    var liveButton = $('a.live');
+    var liveLinkNumber = GM_getValue("liveLinkNumber", 0);
+    console.log(liveLinkNumber + "brabrabr");
+    liveButton = liveButton[liveLinkNumber];
+    if(liveButton == undefined){
+        GM_setValue("liveLinkNumber", 0);
+        liveButton = $('a.live');
+        liveButton = liveButton[0];
+    }
+    liveLinkNumber = GM_getValue("liveLinkNumber", 0);
+    GM_setValue("liveLinkNumber", liveLinkNumber + 1);
     if(liveButton == undefined && window.location.toString().indexOf(redirectPathCheck) == -1){
         return method();
     } else{
@@ -44,7 +79,7 @@ function liveClicker(method, loop){
             if(window.location.toString().indexOf(redirectPathCheck) == -1){
                 liveClicker(function(){window.location.href = 'https://lolesports.com/schedule'}, rewardCheck);
             } else if(rewardsIcon != 'M14.75,8.5 L10.25,13 L8.5,11.25 L7,12.75 L10.25,16 L16.25,10 L14.75,8.5 Z M12,19 C8.14,19 5,15.859 5,12 C5,8.14 8.14,5 12,5 C15.859,5 19,8.14 19,12 C19,15.859 15.859,19 12,19 Z M12,3 C7.029,3 3,7.029 3,12 C3,16.971 7.029,21 12,21 C16.971,21 21,16.971 21,12 C21,7.029 16.971,3 12,3 Z' &&
-                      manualTimer > 10){
+                      manualTimer > 10 && containerLoaded == false){
                 window.location.href = 'https://lolesports.com/schedule';
             } else if(document.readyState == 'complete'){
                 if($('.drops-fulfilled').length){
