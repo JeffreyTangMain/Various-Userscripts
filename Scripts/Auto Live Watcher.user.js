23639c75595b99b7833fc6c63f85f3e8fadacd0e
JeffreyTangMain <97859002+JeffreyTangMain@users.noreply.github.com>
2023-04-22 18:13:20 -0400

    Now reworked to also work with Twitch

--- Scripts/Auto Live Watcher.user.js
+++ Scripts/Auto Live Watcher.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         YouTube Live Watcher
+// @name         Auto Live Watcher
 // @namespace    https://www.youtube.com/
-// @version      1.0.1
-// @description  Watches YouTube live streams automatically as they appear.
+// @version      2.0.0
+// @description  Watches YouTube or Twitch live streams automatically as they appear.
 // @author       Main
 // @match        https://www.youtube.com/*/streams
+// @match        https://www.twitch.tv/*/about
 // @run-at        document-start
 // @require http://code.jquery.com/jquery-3.4.1.min.js
 // @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js
@@ -14,10 +15,14 @@
 
 // Saves where you start this script so you can jump back to it later
 var startingChannel = window.location.href;
-// Waits for streams to load
-waitForKeyElements(".ytd-two-column-browse-results-renderer", mainMethod, false, 3000);
+// Checks for the website you're currently on and runs the appropriate check
+if(window.location.toString().indexOf('youtube.com') != -1) {
+    waitForKeyElements(".ytd-two-column-browse-results-renderer", youTubeMethod, false, 3000);
+} else if(window.location.toString().indexOf('twitch.tv') != -1) {
+    waitForKeyElements(".channel-info-content", twitchMethod, false, 3000);
+}
 
-function mainMethod() {
+function youTubeMethod() {
     // Absurd selector for the live icon in the stream list
     var liveButton = $("ytd-thumbnail-overlay-time-status-renderer.style-scope.ytd-thumbnail[overlay-style='LIVE']");
     // Selects the recommendation screen when a stream ends
@@ -53,4 +58,31 @@ function mainMethod() {
 function returnToLive() {
     // Return to stream list of saved streamer
     window.location.assign(startingChannel);
+}
+
+function twitchMethod() {
+    // Check for live icon below channel profile picture
+    var liveIcon = $('div[class*="ChannelStatusTextIndicator"] [class^="CoreText"]');
+    var offlineText = $('[data-test-selector="follow-panel-overlay"] [class^="CoreText"]');
+    var pauseButton = $('[data-a-target="player-play-pause-button"]');
+
+    if(window.location.toString().indexOf('/about') != -1) {
+        // If on the about page to start, save the URL to return to later
+        startingChannel = window.location.href;
+    }
+
+    if(liveIcon != undefined && liveIcon.text() == "LIVE") {
+        if(offlineText != undefined && offlineText.text().includes("Follow and get notified when")) {
+            // If not live, go back to the about page
+            returnToLive();
+        } else if(pauseButton[0] != undefined && pauseButton.attr("data-a-player-state") == "playing") {
+            // Unpauses the video
+            pauseButton.click();
+        }
+        // If live, click the live icon to join stream
+        liveIcon.click();
+    }
+
+    // Makes waitForKeyElements keep running
+    return true;
 }
