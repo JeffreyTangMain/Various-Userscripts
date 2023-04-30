eceedacff75178ac2d4b5f73f1f3b9294e872a59
JeffreyTangMain <97859002+JeffreyTangMain@users.noreply.github.com>
2023-04-30 08:32:43 -0400

    Added if else, noted known error
    Now the script runs either the embed part or the main part of the script depending on where it is.
    Also the heartbeater seems to stop if the embed is muted, so don't mute it in the embed and use some other extension for it.

--- Scripts/LoL Esports Redirector.user.js
+++ Scripts/LoL Esports Redirector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LoL Esports Redirector
 // @namespace    https://lolesports.com/
-// @version      4.3.3
+// @version      4.4.0
 // @description  Redirects the schedule to the livestream so you're always watching when it's available.
 // @author       Main
 // @match        https://lolesports.com/schedule*
@@ -16,145 +16,149 @@
 // Documenting globals for JSHint to not throw an error for JQuery's $ function
 /* globals $ waitForKeyElements */
 
-var redirectPathCheck = '/live';
-var containerLoaded = false;
-var tempString = '';
-var delayRefreshTimer = 300000;
+if(window.location.toString().indexOf('youtube.com/embed') != -1){
+    // If else statement that makes this script work for the embed or lolesports
+    waitForKeyElements("button.ytp-play-button", autoplayEmbed, false);
 
-var oldLog = unsafeWindow.console.log;
-
-unsafeWindow.console.log = function(msg) {
-    try {
-        // arguments[2] refers to the text before the -> in the console
-        if(arguments[2].includes('RewardsStatusInformer') && arguments[4].includes('stopped')){
-            // arguments[4] includes the heartbeater status update
-            // Refreshes after a delay if the RewardsStatusInformer's heartbeat has stopped
-            window.location.href = 'https://lolesports.com/schedule';
+    function autoplayEmbed(){
+        if($("button.ytp-play-button").attr("data-title-no-tooltip") != "Pause"){
+            $("button.ytp-large-play-button").click();
         }
-        else if(arguments[2].includes('RewardsStatusInformer') && !(arguments[5].includes('mission=on') || arguments[5].includes('drop=on'))){
-            // Checks if any rewards are enabled
-            if(GM_getValue("liveLinkNumber", 0) < GM_getValue("liveGameCount", 1)) {
-                window.location.href = 'https://lolesports.com/schedule';
-            } else if((Date.now() - GM_getValue("currentMinute", 0)) > delayRefreshTimer){
-                window.location.href = 'https://lolesports.com/schedule';
+        return true;
+    }
+} else {
+    var redirectPathCheck = '/live';
+    var containerLoaded = false;
+    var tempString = '';
+    var delayRefreshTimer = 300000;
+
+    var oldLog = unsafeWindow.console.log;
+
+    unsafeWindow.console.log = function(msg) {
+        try {
+            // arguments[2] refers to the text before the -> in the console
+            if(arguments[2].includes('RewardsStatusInformer') && arguments[4].includes('stopped')){
+                // arguments[4] includes the heartbeater status update
+                // Refreshes after a delay if the RewardsStatusInformer's heartbeat has stopped
+                // Note: heartbeater stops if the embed is muted and in the background, make sure you don't mute it in the embed
+                window.location.assign('https://lolesports.com/schedule');
             }
-        }
-        // Checks if the video player has ended, which indicates a VOD
-        else if(arguments[2].includes('VideoPlayer') && arguments[5].includes('ended')){
-            window.location.href = 'https://lolesports.com/schedule';
-        }
-        // Checks if the video player is playing
-        else if(arguments[2].includes('VideoPlayer') && arguments[5].toLowerCase().includes('playing')){
-            containerLoaded = true;
-        }
-        // This is a check specifically for YouTube because it has a different format for the log
-        else if(arguments[2].includes('VideoPlayerYouTube') && arguments[4].toLowerCase().includes('playing')){
-            containerLoaded = true;
-        }
-        // Check for an erroring WatchLive, which is another indicator of the stream ending
-        else if(arguments[2].includes('WatchLive') && arguments[4].length == undefined){
-            window.location.href = 'https://lolesports.com/schedule';
-        }
-    } catch (error) {
-        null;
-    }
-    oldLog.apply(null, arguments);
-}
-
-waitForKeyElements("button.ytp-play-button", autoplayEmbed, false);
-
-function autoplayEmbed(){
-    if($("button.ytp-play-button").attr("data-title-no-tooltip") != "Pause"){
-        $("button.ytp-large-play-button").click();
-    }
-    return true;
-}
-
-if(window.location.toString().indexOf('/schedule') != -1){
-    waitForKeyElements('.Event', mainMethod);
-} else{
-    window.onload = mainMethod;
-}
-
-function mainMethod(){
-    // A refresh function that runs when the live button is undefined and the page is not at the live section
-    // This should refresh when there are no live games to check for new ones every refresh
-    liveClicker(function(){setTimeout(function(){window.location.href = 'https://lolesports.com/schedule'}, delayRefreshTimer)});
-}
-
-function liveClicker(method, loop){
-    // Finds and clicks all leagues that aren't currently enabled
-    var clickedLeagues = $('button.button.league')
-    clickedLeagues.filter(":not('.selected')").each(function() {
-        this.click();
-    })
-    // Loops through all the live buttons in order, and resets back to the start of the list once it reaches the end
-    var liveButton = $('a.live');
-    // ---------------------------------------
-    // This chunk of code manages the userscript's memory of having gone through all the current live links
-    if(window.location.toString().indexOf('/schedule') != -1){
-        var liveGameList = $('a.live');
-        var liveGameLinks = "";
-        liveGameList.each(function() {
-            liveGameLinks = liveGameLinks + this.href;
-        });
-        if(GM_getValue("liveGameLinks", '') != liveGameLinks) {
-            GM_setValue("liveGameCount", liveGameList.length);
-            GM_setValue("liveGameLinks", liveGameLinks);
-            GM_setValue("liveLinkNumber", 0);
-        }
-    }
-    // ---------------------------------------
-    var liveLinkNumber = GM_getValue("liveLinkNumber", 0);
-    liveButton = liveButton[liveLinkNumber];
-    if(liveButton == undefined){
-        GM_setValue("liveLinkNumber", 0);
-        liveButton = $('a.live');
-        liveButton = liveButton[0];
-    }
-    liveLinkNumber = GM_getValue("liveLinkNumber", 0);
-    GM_setValue("liveLinkNumber", liveLinkNumber + 1);
-
-    //Prepares the timer when the page is loaded to refresh if there are no rewards.
-    GM_setValue("currentMinute", Date.now());
-
-    if(liveButton == undefined && window.location.toString().indexOf(redirectPathCheck) == -1){
-        return method();
-    } else{
-        if(liveButton != undefined){
-            liveButton.click();
-        }
-        if(loop != undefined){
-            clearInterval(loop);
-        }
-        var manualTimer = 0;
-        var timerThreshold = 60;
-        var rewardsEnabled = false;
-        var rewardCheck = setInterval(function() {
-            manualTimer > timerThreshold ? null : manualTimer++;
-            var rewardsIcon = $('.RewardsStatusInformer .status-summary svg path').attr('fill');
-            if(rewardsIcon == '#5ABBD4'){
-                rewardsEnabled = true;
-            }
-            if(window.location.toString().indexOf(redirectPathCheck) == -1){
-                // Resolves issues if the live stream is clicked out of at any time
-                // Will return to the schedule page if clicked into a page with no live button
-                // If there is a live button, meaning you're on the schedule, it will click it, remove the old loop, and then continue normal operation
-                liveClicker(function(){window.location.href = 'https://lolesports.com/schedule'}, rewardCheck);
-            } else if((rewardsEnabled == true && rewardsIcon != '#5ABBD4') || (manualTimer > timerThreshold && (containerLoaded == false || rewardsEnabled == false))){
-                // The first check is if rewards were enabled at some point in the past and aren't enabled currently
-                // The second check is a backup wait for some seconds that will refresh the page if the video still hasn't loaded
-                // #5ABBD4 is the fill color when rewards are working, #DE2F2F is the fill color when rewards aren't
-                window.location.href = 'https://lolesports.com/schedule';
-            } else if(document.readyState == 'complete'){
-                // Should click the close button on any drop popups
-                if($('.drops-fulfilled').length){
-                    var closeReward = document.querySelector (
-                        '.drops-fulfilled .actions .close'
-                    );
-                    closeReward.click();
+            else if(arguments[2].includes('RewardsStatusInformer') && !(arguments[5].includes('mission=on') || arguments[5].includes('drop=on'))){
+                // Checks if any rewards are enabled
+                if(GM_getValue("liveLinkNumber", 0) < GM_getValue("liveGameCount", 1)) {
+                    window.location.assign('https://lolesports.com/schedule');
+                } else if((Date.now() - GM_getValue("currentMinute", 0)) > delayRefreshTimer){
+                    window.location.assign('https://lolesports.com/schedule');
                 }
             }
-        }, 1000);
+            // Checks if the video player has ended, which indicates a VOD
+            else if(arguments[2].includes('VideoPlayer') && arguments[5].includes('ended')){
+                window.location.assign('https://lolesports.com/schedule');
+            }
+            // Checks if the video player is playing
+            else if(arguments[2].includes('VideoPlayer') && arguments[5].toLowerCase().includes('playing')){
+                containerLoaded = true;
+            }
+            // This is a check specifically for YouTube because it has a different format for the log
+            else if(arguments[2].includes('VideoPlayerYouTube') && arguments[4].toLowerCase().includes('playing')){
+                containerLoaded = true;
+            }
+            // Check for an erroring WatchLive, which is another indicator of the stream ending
+            else if(arguments[2].includes('WatchLive') && arguments[4].length == undefined){
+                window.location.assign('https://lolesports.com/schedule');
+            }
+        } catch (error) {
+            null;
+        }
+        oldLog.apply(null, arguments);
+    }
+
+    if(window.location.toString().indexOf('/schedule') != -1){
+        waitForKeyElements('.Event', mainMethod);
+    } else{
+        window.onload = mainMethod;
+    }
+
+    function mainMethod(){
+        // A refresh function that runs when the live button is undefined and the page is not at the live section
+        // This should refresh when there are no live games to check for new ones every refresh
+        liveClicker(function(){setTimeout(function(){window.location.assign('https://lolesports.com/schedule')}, delayRefreshTimer)});
+    }
+
+    function liveClicker(method, loop){
+        // Finds and clicks all leagues that aren't currently enabled
+        var clickedLeagues = $('button.button.league')
+        clickedLeagues.filter(":not('.selected')").each(function() {
+            this.click();
+        })
+        // Loops through all the live buttons in order, and resets back to the start of the list once it reaches the end
+        var liveButton = $('a.live');
+        // ---------------------------------------
+        // This chunk of code manages the userscript's memory of having gone through all the current live links
+        if(window.location.toString().indexOf('/schedule') != -1){
+            var liveGameList = $('a.live');
+            var liveGameLinks = "";
+            liveGameList.each(function() {
+                liveGameLinks = liveGameLinks + this.href;
+            });
+            if(GM_getValue("liveGameLinks", '') != liveGameLinks) {
+                GM_setValue("liveGameCount", liveGameList.length);
+                GM_setValue("liveGameLinks", liveGameLinks);
+                GM_setValue("liveLinkNumber", 0);
+            }
+        }
+        // ---------------------------------------
+        var liveLinkNumber = GM_getValue("liveLinkNumber", 0);
+        liveButton = liveButton[liveLinkNumber];
+        if(liveButton == undefined){
+            GM_setValue("liveLinkNumber", 0);
+            liveButton = $('a.live');
+            liveButton = liveButton[0];
+        }
+        liveLinkNumber = GM_getValue("liveLinkNumber", 0);
+        GM_setValue("liveLinkNumber", liveLinkNumber + 1);
+
+        //Prepares the timer when the page is loaded to refresh if there are no rewards.
+        GM_setValue("currentMinute", Date.now());
+
+        if(liveButton == undefined && window.location.toString().indexOf(redirectPathCheck) == -1){
+            return method();
+        } else{
+            if(liveButton != undefined){
+                liveButton.click();
+            }
+            if(loop != undefined){
+                clearInterval(loop);
+            }
+            var manualTimer = 0;
+            var timerThreshold = 60;
+            var rewardsEnabled = false;
+            var rewardCheck = setInterval(function() {
+                manualTimer > timerThreshold ? null : manualTimer++;
+                var rewardsIcon = $('.RewardsStatusInformer .status-summary svg path').attr('fill');
+                if(rewardsIcon == '#5ABBD4'){
+                    rewardsEnabled = true;
+                }
+                if(window.location.toString().indexOf(redirectPathCheck) == -1){
+                    // Resolves issues if the live stream is clicked out of at any time
+                    // Will return to the schedule page if clicked into a page with no live button
+                    // If there is a live button, meaning you're on the schedule, it will click it, remove the old loop, and then continue normal operation
+                    liveClicker(function(){window.location.assign('https://lolesports.com/schedule')}, rewardCheck);
+                } else if((rewardsEnabled == true && rewardsIcon != '#5ABBD4') || (manualTimer > timerThreshold && (containerLoaded == false || rewardsEnabled == false))){
+                    // The first check is if rewards were enabled at some point in the past and aren't enabled currently
+                    // The second check is a backup wait for some seconds that will refresh the page if the video still hasn't loaded
+                    // #5ABBD4 is the fill color when rewards are working, #DE2F2F is the fill color when rewards aren't
+                    window.location.assign('https://lolesports.com/schedule');
+                } else if(document.readyState == 'complete'){
+                    // Should click the close button on any drop popups
+                    if($('.drops-fulfilled').length){
+                        var closeReward = document.querySelector (
+                            '.drops-fulfilled .actions .close'
+                        );
+                        closeReward.click();
+                    }
+                }
+            }, 1000);
+        }
     }
 }
