2c7bf47c719b115649950279fa85a2ceeb284dd1
JeffreyTangMain <97859002+JeffreyTangMain@users.noreply.github.com>
2025-12-30 16:36:37 -0500

    Create Add Block Button.user.js

---
+++ Scripts/Add Block Button.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Add Block button
+// @namespace    https://github.com/
+// @version      1.0
+// @description  Adds block button to comments and posts
+// @author       Main
+// @match        https://*.reddit.com/*
+// @exclude      https://*.reddit.com/user/*
+// @exclude      https://*.reddit.com/message/inbox/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+// Documenting globals for JSHint to not throw an error for JQuery's $ function
+/* globals $ */
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+
+setInterval(addBlocks,1000);
+//addBlocks();
+
+function addBlocks() {
+    $(".tagline .author").each(function(index){
+        if(this.closest("div").getElementsByClassName("flat-list").length != 0 && !this.closest("div").getElementsByClassName("flat-list")[0].lastChild.lastChild.className.includes("block_user-button")){
+            this.closest("div").getElementsByClassName("flat-list")[0].insertAdjacentHTML("beforeend",'<li><form class="toggle block_user-button " action="#" method="get"><input type="hidden" name="executed" value="blocked"><input type="hidden" name="account_id" value="'+this.className.split(" ")[2].replace("id-","")+'"><span class="option main active"><a href="#" class="togglebutton access-required" onclick="return toggle(this)">block user</a></span><span class="option error">are you sure?  <a href="javascript:void(0)" class="yes" onclick="change_state(this, &quot;block_user&quot;, null, undefined, null)">yes</a> / <a href="javascript:void(0)" class="no" onclick="return toggle(this)">no</a></span></form></li>');
+        }
+    });
+}
