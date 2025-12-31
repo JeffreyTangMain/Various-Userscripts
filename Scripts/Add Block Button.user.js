d8ce2add63945fcf3670e3ba67a58ee6729c5dd7
JeffreyTangMain <97859002+JeffreyTangMain@users.noreply.github.com>
2025-12-31 11:04:13 -0500

    Improved check for button existing
    Now uses jquery instead of raw js to check the last element

--- Scripts/Add Block Button.user.js
+++ Scripts/Add Block Button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add Block button
 // @namespace    https://github.com/
-// @version      1.1
+// @version      1.2
 // @description  Adds block button to comments and posts
 // @author       Main
 // @match        https://*.reddit.com/*
@@ -14,14 +14,14 @@
 /* globals $ */
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 
-setInterval(addBlocks,1000);
+setInterval(addBlocks,2000);
 //addBlocks();
 
 function addBlocks() {
     $(".tagline .author").each(function(index){
-        if(this.closest("div").getElementsByClassName("flat-list").length != 0 && !this.closest("div").getElementsByClassName("flat-list")[0].lastChild.lastChild.className.includes("block_user-button")){
+        if(this.closest("div").getElementsByClassName("flat-list").length != 0 && $(this).closest("div").find(".flat-list").find("#userscript-block-button").length <= 0){
             let id = this.className.split(" ").find((element) => element.includes("id-")).replace("id-","");
-            this.closest("div").getElementsByClassName("flat-list")[0].insertAdjacentHTML("beforeend",'<li><form class="toggle block_user-button " action="#" method="get"><input type="hidden" name="executed" value="blocked"><input type="hidden" name="account_id" value="'+id+'"><span class="option main active"><a href="#" class="togglebutton access-required" onclick="return toggle(this)">block '+id+'</a></span><span class="option error">are you sure?  <a href="javascript:void(0)" class="yes" onclick="change_state(this, &quot;block_user&quot;, null, undefined, null)">yes</a> / <a href="javascript:void(0)" class="no" onclick="return toggle(this)">no</a></span></form></li>');
+            this.closest("div").getElementsByClassName("flat-list")[0].insertAdjacentHTML("beforeend",'<li id="userscript-block-button"><form class="toggle block_user-button " action="#" method="get"><input type="hidden" name="executed" value="blocked"><input type="hidden" name="account_id" value="'+id+'"><span class="option main active"><a href="#" class="togglebutton access-required" onclick="return toggle(this)">block '+id+'</a></span><span class="option error">are you sure?  <a href="javascript:void(0)" class="yes" onclick="change_state(this, &quot;block_user&quot;, null, undefined, null)">yes</a> / <a href="javascript:void(0)" class="no" onclick="return toggle(this)">no</a></span></form></li>');
         }
     });
 }
